89f76aac8d813df9ef81f6ea4019c5e9544236b4
Taylor Bruno <taylorbruno895@gmail.com>
2020-01-21 14:41:39 -0500

    added handlebars html and css

--- db/schema.sql
+++ db/schema.sql
@@ -6,6 +6,6 @@ USE burgers_db;
 CREATE TABLE burgers (
     id INT NOT NULL AUTO_INCREMENT,
     burger_name VARCHAR(150),
-    devoured BOOLEAN,
+    devoured BOOLEAN DEFAULT false,
     PRIMARY KEY(id)
 );
