bf77e5c805c3b0cff03f7eb1634ebde5655d77f6
Taylor Bruno <taylorbruno895@gmail.com>
2020-01-22 14:30:00 -0500

    added onclick functions. still receiving 404 error on post

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,6 +1,33 @@
 $(function() {
-    $(".liDevBtn").on("click", function(event) {
+    $(document).on("click", ".liDevBtn", function(event) {
         let id = $(this).data("id");
-        let newEaten = $(this).data("neweaten");
+        let newEaten = $(this).data("devoured");
+        let newEatenState = {
+            devoured: newEaten
+        };
+        $.ajax("/api/burgers/" + id, {
+            type: "PUT",
+            data: newEatenState
+        }).then(function() {
+            console.log("Changed devoured to", newEaten);
+            location.reload();
+        });
+    });
+
+    $(document).on("click", "#newBurg",function(event) {
+        event.preventDefault();
+
+        let newBurger = {
+            name: $("#burgTextarea").val().trim()
+        };
+
+        console.log(newBurger);
+        $.ajax("/api/burgers", {
+            type: "POST",
+            data: newBurger
+        }).then(function() {
+            console.log("Created new burger");
+            location.reload();
+        });
     });
 });
